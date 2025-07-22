fac7fc169b3a35f324c76d38cbb3752ec3345f42
Fernando IL <luffynando@gmail.com>
2025-07-22 11:10:53 -0600

    chore: :green_heart: configure use of github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aws-quicksight.github.io',
+  base: '/docs',
   integrations: [
     starlight({
       title: 'Factorbi QuickSight',
